d766a31b10fa4dcd460338785922666acced21e0
taichanNE30 <ne300209@senshu-u.jp>
2019-03-07 15:32:11 +0900

    UI統合/ファイル整理

---
+++ Assets/Scripts/GitKeepMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 新規フォルダ作成時に.gitkeepを自動作成
+/// </summary>
+public class GitkeepMaker : AssetPostprocessor
+{
+    private readonly static string folderKeeperName = ".gitkeep";
+
+    public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetsPath)
+    {
+        if (!Directory.Exists(".git")) return;
+
+        foreach (string path in importedAssets)
+        {
+            if (Directory.Exists(path))
+            {
+                string folderKeeperPath = path + "/" + folderKeeperName;
+                if (!File.Exists(folderKeeperPath))
+                {
+                    File.Create(folderKeeperPath).Close();
+                    Debug.Log(folderKeeperPath + " created");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MazaiController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazaiController : MonoBehaviour {
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			// Transform.FindObjectOfType<GameController>().FoundMazai();
+			Destroy(this.gameObject);
+		}
+	}
+}
