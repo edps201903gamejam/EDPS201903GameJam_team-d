492ab2b7ae1989363be36d1e7181394699e5b7aa
clon <guyclon.git@gmail.com>
2019-03-08 12:35:48 +0900

    スコア機能の追加。

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-	private int currentStageNum;//0はタイトル
+	public int currentStageNum;//0はタイトル
 	private float timer;
 	private int mazaiSum;
 	private int CurrentAnimalNum;
+	private Text stageText;
+	private Text timerText;
 	private GameObject[] animalUI = new GameObject[5];
 	private GameObject[] animalIcon = new GameObject[5]; //0は人なので使用しない
 	private GameObject animalMode;
@@ -20,6 +23,7 @@ public class GameController : MonoBehaviour {
 	private bool[] _foundAnimal = new bool[5];
 	private bool[] _clearStage = new bool[3];
 	public int DisplayUI = -1;//-1だったら表示されていない。
+
 	void Awake() {
 		if(1 < GameObject.FindGameObjectsWithTag("GameController").Length) {
 			Destroy(this.gameObject);
@@ -32,23 +36,35 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Update () {
-		timer += Time.deltaTime;
+		if (currentStageNum > 0 && timerText) {
+			timer += Time.deltaTime;
+			timerText.text = ((int)(timer / 60)).ToString("D2") + ":" + ((int)(timer % 60)).ToString("D2");
+		}
 	}
 
 	//新しい動物を見つけたときにプレイヤーが実行
 	public void FoundAnimal(int a) {
 		_foundAnimal[a] = true;
-		DisplayUI = a;
-		if(DisplayUI != -1) {
+		if (DisplayUI != -1) {
 			animalUI[DisplayUI].SetActive(false);
 		}
+		DisplayUI = a;
 		animalUI[a].SetActive(true);
 		animalIcon[a].SetActive(true);
 	}
 
 	//魔剤を見つけたときにプレイヤーが実行
 	public void FoundMazai() {
-		timer -= 10;
+		if (timer < 10) timer = 0;
+		else timer -= 10;
+		if(mazaiSum == 0) {
+			if (DisplayUI != -1) {
+				animalUI[DisplayUI].SetActive(false);
+			}
+			DisplayUI = 0;
+			animalUI[0].SetActive(true);
+			
+		}
 		mazaiSum++;
 
 	}
@@ -72,35 +88,47 @@ public class GameController : MonoBehaviour {
 
 	//シーンの読み込みが終わったら、実行される関数
 	void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-		Debug.Log(currentStageNum);
+		Debug.Log(scene.name);
 		if (currentStageNum == 0) {
 			GameObject SS = GameObject.Find("StageSelect");
 			SS.GetComponent<StageSelectController>().SetStageSelect();
-		}
-		if (0 < currentStageNum) {
-			Debug.Log(scene.name + " scene loaded");
-			Debug.Log(SceneManager.GetActiveScene());
+		}else if (scene.name != "GameClear" && scene.name != "GameOver" && 0 < currentStageNum) {
+			stageText = GameObject.Find("Canvas/PlayerStatus/StageText").GetComponent<Text>();
+			stageText.text += currentStageNum.ToString();
+			timerText = GameObject.Find("Canvas/PlayerStatus/TimerText").GetComponent<Text>();
+			timer = 0;
+			timerText.text = "00:00";
 			playerAnimetor = GameObject.Find("kyoko0").GetComponent<Animator>();
 			animalMode = GameObject.Find("Canvas/PlayerStatus/ModeIcon");
 			GameObject au = GameObject.Find("Canvas/Animal");
 			GameObject ai = GameObject.Find("Canvas/PlayerStatus/AnimalIcon");
 			for (int i = 0; i < 5; i++) {
 				animalUI[i] = au.transform.GetChild(i).gameObject;
-				if (i != 4) animalIcon[i+1] = ai.transform.GetChild(i).gameObject;
+				if (i != 4) {
+					animalIcon[i + 1] = ai.transform.GetChild(i).gameObject;
+					if (_foundAnimal[i + 1]) {
+						animalIcon[i + 1].SetActive(true);
+					}
+				}
 			}
+			if(mazaiSum == 0) {
+				animalUI[0].SetActive(true);
+				DisplayUI = 0;
+			}
+		}else if(scene.name == "GameClear" || scene.name == "GameOver") {
+			Text scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+			scoreText.text = timerText.text;
 		}
 	}
 
 
 	public void GameClear() {
-		currentStageNum = -1;
 		_clearStage[currentStageNum-1] = true;
 		SceneManager.LoadScene("GameClear", LoadSceneMode.Additive);
 		Time.timeScale = 0;
 	}
 
 	public void GameOver() {
-		currentStageNum = -1;
 		SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
 		Time.timeScale = 0;
 	}
--- Assets/Scripts/NextStageButton.cs
+++ Assets/Scripts/NextStageButton.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RetryButton : MonoBehaviour {
+public class NextStageButton : MonoBehaviour {
 	GameObject GC;
 	public void OnClick() {
 		Time.timeScale = 1;
 		GC = GameObject.Find("GameController");
 		GameController gameController = GC.GetComponent<GameController>();
-		gameController.ChangeStage(gameController.currentStageNum);
+		gameController.ChangeStage(gameController.currentStageNum+1);
 	}
 }
